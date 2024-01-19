e16a50a390b607214cd6e2ddc15ac27bb7723796
ValRom <valentin.romanet@etu.univ-orleans.fr>
2024-01-19 10:46:45 +0100

    feat : affichage correction

--- Classes/Correction.php
+++ Classes/Correction.php
@@ -1,8 +1,6 @@
 <?php
 
-namespace Classes\Types;
-
-class Correction{
+class Correction {
 
     protected $questions;
     protected $answers;
@@ -15,9 +13,12 @@ class Correction{
 
     public function display(): string {
         $html = "<h1>Correction</h1>";
+        $cpt = 0;
         foreach ($this->questions as $question) {
-            $html .= "<div class='question'>";
-            $html .= "<h2>Question {$question->getId()}</h2>";
+            $cpt++;
+            $html .= "<section class='reponses'>";
+            $html .= "<div class='bloc_reponse'>";
+            $html .= "<h2>Question {$cpt}</h2>";
             $html .= "<p>Votre réponse : {$this->answers[$question->getId()]}</p>";
             $html .= "<p>La bonne réponse était : {$question->getAnswer()}</p>";
             if ($question->getAnswer() == $this->answers[$question->getId()]) {
@@ -26,6 +27,7 @@ class Correction{
                 $html .= "<p class='incorrect'>Mauvaise réponse</p>";
             }
             $html .= "</div>";
+            $html .= "</section>";
         }
         $html .= "<p>Votre score : {$this->score}/".count($this->questions)."</p>";
         return $html;
